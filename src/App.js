7e0795701c0245938829bbabb1b45f074a5c555d
이가은 <gaeun.lee@mirero.co.kr>
2023-09-19 16:52:48 +0900

    component와 prop-types 학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
+function Food({ name, picture, rating }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>I like {name}</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} />
     </div>
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const foodLike = [
+  {
+    id: 1,
+    name: "sanchou",
+    image: "사진1",
+    rating: 4.9,
+  },
+  {
+    id: 2,
+    name: "babo",
+    image: "사진2",
+    rating: 4.6,
+  },
+  {
+    id: 3,
+    name: "mungmung",
+    image: "사진3",
+    rating: 3.1,
+  },
+];
+
+function renderFood(wow) {
+  return (
+    <Food
+      key={wow.id}
+      name={wow.name}
+      picture={wow.image}
+      rating={wow.rating}
+    />
+  );
+}
+
+function App() {
+  return <div>{foodLike.map(renderFood)}</div>;
 }
 
 export default App;
