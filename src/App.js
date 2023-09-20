ca75a7081cee20e8d06780ab0544e4ac51e58575
이가은 <gaeun.lee@mirero.co.kr>
2023-09-20 10:22:49 +0900

    리액트 개념 학습 끝

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I like {name}</h3>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  componentDidMount() {
+    // 영화 데이터 로딩
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
 
-const foodLike = [
-  {
-    id: 1,
-    name: "sanchou",
-    image: "사진1",
-    rating: 4.9,
-  },
-  {
-    id: 2,
-    name: "babo",
-    image: "사진2",
-    rating: 4.6,
-  },
-  {
-    id: 3,
-    name: "mungmung",
-    image: "사진3",
-    rating: 3.1,
-  },
-];
-
-function renderFood(wow) {
-  return (
-    <Food
-      key={wow.id}
-      name={wow.name}
-      picture={wow.image}
-      rating={wow.rating}
-    />
-  );
-}
-
-function App() {
-  return <div>{foodLike.map(renderFood)}</div>;
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+  }
 }
 
 export default App;
